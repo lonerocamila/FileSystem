71f5a6e7f03627d326e04a36562d0a277a0ccdbb
Camila Lo Nero <lonerocamila@gmail.com>
2023-04-16 21:43:45 -0300

    entrega terminada

--- main.js
+++ main.js
@@ -1,130 +1,134 @@
-import * as fs from 'fs' //importo solamente promesas  
-import { deleteModel } from 'mongoose';
-
+import { promises as fs } from 'fs' //importo solamente promesas  
 class ProductManager{
 
     constructor(){
         this.path = this.path //mediante el path hago todas las operaciones del txt
         this.products = [];
-        // id autoincrementable para cada producto con getProductById 
-        this.id = 0;
+        
     }
-    validate(product){
-        if(
-            !product.title ||
-            !product.description ||
-            !product.price ||
-            !product.thumbnail ||
-            !product.code ||
-            !product.stock ||
-            typeof product.title !== "string" ||
-            typeof product.description !== "string" ||
-            typeof product.code !== "string" ||
-            typeof product.thumbnail !== "string" ||
-            typeof product.price !== "number" ||
-            typeof product.stock !== "number"
-          )
-            return console.log("Not Validate");
-        }
+      
+    validate(title, description, thumbnail, price, code, stock, id){
+        
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.thumbnail = thumbnail;
+            this.code = code;
+            this.stock = stock;
 
- async  addProduct (product) {
-        this.validate(product)
-        this.products.length === 0 ? (id = 1) : (id = this.products[this.products.length - 1].id + 1);
+        } 
 
-    if (this.products.some((e) => e.code === product.code)) {
-      console.log("code already entered ");
+  static incrementId() {
+    if (this.incrementId) {
+       this.incrementId++
+          } else {
+           this.incrementId = 1
+          }
+          return this.incrementId
+       }
+     
+//poner el await en el addproduct y modificar para que ande bien el id
+async addProduct (product) {
+
+    if (this.products.find((product.code == product.code)) ) {
+     return("product already exist ");
      } else {
-        const newProduct = {
-            id: this.id,
-            title: product.title,
-            description: product.description,
-            price: product.price,
-            thumbnail: product.thumbnail,
-            code: product.code,
-            stock: product.stock,
-      };
-      this.products.push(newProduct);
-      return newProduct;
+      const prodsJSON = await fs.readFile(this.path, 'utf-8')
+      const product = JSON.parse(prodsJSON)
+      product.id = ProductManager.incrementId() //al añadir un producto hacemos que incremente el id 
+      prods.push(products)
+      await fs.writeFile(this.path, JSON.stringify(prods))
+      return 'product was created';
       }
+      
     }
     
+    //getProduct debe devolver un arreglo vacio
    async getProduct(){
-        const products = await fs.readFile(this.path, 'utf-8')
-         
-        const prods = JSON.parse(products)
+        //consulto el array del txt 
+       const products = await fs.readFile(this.path, 'utf-8')
+       const prods = JSON.parse(products)
+       console.log(prods)
         
-        return products;
+       return products
     }
 
-    getProductById(id){
-     const productById = this.products.find((idProduct)=> idProduct.id === id);
-     !productById ? console.log("Not found") : console.log(productById);
+   async   getProductById(id){
+     const prodsJSON =   await fs.readFile(this.path, 'utf-8')
+     const prods = JSON.parse(prodsJSON)
+     const product = this.products.find(producto => producto.id == id);
+
+     product ? console.log("Not found") : console.log(product);
        
       return productById;
     }
-  async  updateProduct()
-   
- async deleteProduct (id) {
-}
+
+
+    async updateProduct (id,{title, description, price, code, thumbnail, stock}){
+      const prodsJSON = await fs.readFile(this.path, 'utf-8')
+      const prods = JSON.parse(prodsJSON)
+
+      if (prods.some(prod => prod.id == parseInt(id))) {
+        let index = prods.findIndex (prod => prod.id == parseInt(id))
+        prods[index].title = title
+        prods[index].description = description
+        prods[index].price = price 
+        prods[index].thumbnail = thumbnail
+        prods[index].code = code
+        prods[index].stock = stock
+        await fs.writeFile(this.path, JSON.stringify, (prods))
+        return 'product updated'
+      } else {
+        return 'product was not updated'
+      }
+      
+   }
+
+   async delateProduct(product){
+    const prodsJSON = await fs.readFile(this.path, 'utf-8')
+    const prods = JSON.parse(prodsJSON)
+    if(prods.some(prod => prod.id === parseInt(id))){
+      const prodsFiltrados = prods.filter(prod !== parseint)
+        await fs.writeFile(this.path, JSON.stringify(prodsFiltrados))
+    
+    return 'product delated'
+       }else{
+         return 'product was not deleted'
+   }
+  
   }
+}
 
+const product = new ProductManager('./info.txt') //inserte la ruta al archivo txt, esta me permite guardar y ejecutar mis archivos
 
-const product = new ProductManager('./products.txt'); //inserte la ruta al archivo txt, esta me permite guardar y ejecutar mis archivos
+product.getProduct().then(prods => console.log(prods))
 
-product.addProduct({
+ProductManager.addProduct(product1)
+ProductManager.addProduct(product2)
+const product1 = {
     title: "Remera",
     description: "Remera de algodon",
     price: 1000,
     thumbnail: "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/remera-negra-remera-hombre-remera-basica-21-6952776c38b5e6844216537450098865-640-0.webp",
     stock: 10
-  },
-);
+ 
+};
 
-product.addProduct({
+const product2 = {
 
   title:"camisa",
   description: "camisa larga",
   price: 2000,
   thumbnail: "https://http2.mlstatic.com/D_NQ_NP_852996-MLA53168359304_012023-W.jpg",
   stock: 14
-});
+};
 
-product.addProduct({
+const product3 = {
   title:"pantalon",
   description: "pantalon de jean",
   price: 3000,
   thumbnail: "https://http2.mlstatic.com/D_NQ_NP_637588-MLA49328528400_032022-W.jpg",
   stock: 20
-});
+};
 
-const products = []
  
-const queryTxt = async (path) => {
-  try{
-    await fs.writeFile(path, JSON.stringify(products))
-    let content = await fs.readFile(path, 'utf-8')
-    const aux = JSON.parse(content)
-    aux.push(prod1)
-    console.log (aux)
-    await fs.appendFile(path, JSON.stringify(aux))
-    content =  await fs.readFile (path, 'utf-8')
-    console.log(content1)
-    // await fs.unlink(path)
-  }
-  catch(error){
-    await fs.writeFile(ruta, JSON.stringify((products)))
-    let content = await JSON.parse(fs)
-  }
-  
-}
-
-queryTxt(products.txt)
-
-// const ProductManager = new ProductManager ('./info.txt')
-// ProductManager.addProduct(product)
-// console.log (ProductManager.addProduct(product))
-// console.log(ProductManager.getProductById(2))
-// console.log(ProductManager.getProductById(4))
-// console.log(ProductManager.getProduct())
-
-await ProductManager.getProducts()
